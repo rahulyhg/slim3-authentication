bf48b94ba71ce1977bdaced6d436670a8b7ff2c7
Andrew <contact@andrewdyer.rocks>
2017-06-28 15:28:19 +0100

    Password reset updates

--- www/app/Controller/Auth/Password.php
+++ www/app/Controller/Auth/Password.php
@@ -17,9 +17,7 @@ class Password extends Controller {
         if (!$recoverHash) {
             return($this->redirect("auth.login"));
         }
-        return($this->render("auth/password/reset.twig", [
-            "recover_hash" => $recoverHash
-        ]));
+        return($this->render("auth/password/reset.twig", ["recover_hash" => $recoverHash]));
     }
 
     public function postForgot() {
@@ -30,48 +28,44 @@ class Password extends Controller {
             $emailOrUsername = $this->param("email_or_username");
             $user = User::where("email", $emailOrUsername)->orWhere("username", $emailOrUsername)->first();
             if ($user) {
-                $recoverHash = $this->hash()->unique();
                 $user->update([
-                    "recover_hash" => $recoverHash,
+                    "recover_hash" => ($recoverHash = $this->hash()->unique()),
                 ]);
-                $this->flash("success", $this->text(""));
-                return($this->redirect("auth.login"));
             }
+            $this->flash("success", $this->text("user/forgot_password_success"));
+            return($this->redirect("auth.login"));
         }
         return($this->redirect("auth.password.forgot"));
     }
 
     public function postReset() {
-        $recoverHash = $this->param("recover_hash");
-        if ($recoverHash) {
-            
-        }
         $validation = $this->validate([
             "email_or_username" => v::notEmpty(),
             "new_password" => v::max(8)->notEmpty()->noWhitespace()->identical($this->param("new_password_repeat")),
             "new_password_repeat" => v::max(8)->notEmpty()->noWhitespace()->identical($this->param("new_password"))
         ]);
-        if ($validation->passed()) {
+        $recoverHash = $this->param("recover_hash");
+        if ($validation->passed() and $recoverHash) {
             $emailOrUsername = $this->param("email_or_username");
             $user = User::where("email", $emailOrUsername)->orWhere("username", $emailOrUsername)->first();
             if ($user) {
-                
-                
-                
-                
-                
-                
-                
+                $recoverHash = $this->param("recover_hash");
+                if ($recoverHash === $user->recover_hash) {
+                    $user->update([
+                        "recover_hash" => "",
+                        "salt" => ($salt = $this->hash()->salt(32)),
+                        "password" => $this->hash()->generate($this->param("new_password"), $salt)
+                    ]);
+                    $this->flash("success", $this->text("user/reset_password_success"));
+                    return($this->redirect("auth.login"));
+                }
                 $user->update([
-                    "recover_hash" => "",
-                    "salt" => ($salt = $this->hash()->salt(32)),
-                    "password" => $this->hash()->generate($this->param("password"), $salt)
+                    "recover_hash" => ""
                 ]);
-                                $this->flash("success", $this->text(""));
-                return($this->redirect("auth.login"));
+                $this->flash("danger", $this->text("user/reset_password_failed"));
             }
         }
-       return($this->redirect("auth.password.forgot"));
+        return($this->redirect("auth.password.forgot"));
     }
 
 }
--- www/config/text.php
+++ www/config/text.php
@@ -11,10 +11,16 @@ return [
         ],
         "user" => [
             "account_updated" => "Your account has been successfully updated!",
+            "forgot_password_success" => "If your email is valid, you'll receive an email with instructions on how to reset your password.",
             "password_invalid" => "The current password you have entered is incorrect!",
             "password_updated" => "Your password has been successfully updated!",
             "profile_updated" => "Your profile has been successfully updated!",
-            "" => ""
+            "reset_password_failed" => "Your password could not be reset at this time.",
+            "reset_password_success" => "You have successfully reset your password. You can now login with your new password.",
+            "" => "",
         ]
     ]
+
+    
+    
 ];
